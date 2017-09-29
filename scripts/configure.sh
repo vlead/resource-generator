5ff9978867e4f9ab45310a6b0e4a6dd4d4522915
S Shashank <s.shashank2401@gmail.com>
2017-09-30 04:00:24 +0530

    Update scripts

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "server {" > /etc/nginx/sites-enabled/rg
-echo "    location / {" >> /etc/nginx/sites-enabled/rg
-echo "        proxy_pass http://127.0.0.1:8000;" >> /etc/nginx/sites-enabled/rg
-echo "        proxy_set_header Host \$host;" >> /etc/nginx/sites-enabled/rg
-echo "        proxy_set_header X-Real-IP \$remote_addr;" >> /etc/nginx/sites-enabled/rg
-echo "    }" >> /etc/nginx/sites-enabled/rg
-echo "}" >> /etc/nginx/sites-enabled/rg
+echo "server {" > /etc/nginx/sites-enabled/resource-generator
+echo "    location / {" >> /etc/nginx/sites-enabled/resource-generator
+echo "        proxy_pass http://127.0.0.1:8000;" >> /etc/nginx/sites-enabled/resource-generator
+echo "        proxy_set_header Host \$host;" >> /etc/nginx/sites-enabled/resource-generator
+echo "        proxy_set_header X-Real-IP \$remote_addr;" >> /etc/nginx/sites-enabled/resource-generator
+echo "    }" >> /etc/nginx/sites-enabled/resource-generator
+echo "}" >> /etc/nginx/sites-enabled/resource-generator
 echo "import sys, os" > /var/www/resource-generator/code/wsgi.py
 echo "from runtime.rest.app import create_app" >> /var/www/resource-generator/code/wsgi.py
 echo "from runtime.config import app_config as config" >> /var/www/resource-generator/code/wsgi.py
