0bd212344c5546d6472ae5c91a89148b2331a5c1
S Shashank <s.shashank2401@gmail.com>
2017-09-29 23:07:45 +0530

    Added scripts folder and ADS functionalilty

---
+++ scripts/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "server {" > /etc/nginx/sites-enabled/rg
+echo "    location / {" >> /etc/nginx/sites-enabled/rg
+echo "        proxy_pass http://127.0.0.1:8000;" >> /etc/nginx/sites-enabled/rg
+echo "        proxy_set_header Host \$host;" >> /etc/nginx/sites-enabled/rg
+echo "        proxy_set_header X-Real-IP \$remote_addr;" >> /etc/nginx/sites-enabled/rg
+echo "    }" >> /etc/nginx/sites-enabled/rg
+echo "}" >> /etc/nginx/sites-enabled/rg
+echo "import sys, os" > /var/www/resource-generator/code/wsgi.py
+echo "from runtime.rest.app import create_app" >> /var/www/resource-generator/code/wsgi.py
+echo "from runtime.config import app_config as config" >> /var/www/resource-generator/code/wsgi.py
+echo "application = create_app(config)" >> /var/www/resource-generator/code/wsgi.py
+echo "if __name__ == '__main__':" >> /var/www/resource-generator/code/wsgi.py
+echo "    application.run(host='0.0.0.0')" >> /var/www/resource-generator/code/wsgi.py
