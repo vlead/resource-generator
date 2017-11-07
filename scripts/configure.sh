fbe6e54244ccd574c389970f44b66210d5522810
S Shashank <s.shashank2401@gmail.com>
2017-11-07 23:35:37 +0530

    Added code for proxy

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -9,6 +9,8 @@ echo "}" >> /etc/nginx/sites-enabled/resource-generator
 echo "import sys, os" > /var/www/resource-generator/code/wsgi.py
 echo "from runtime.rest.app import create_app" >> /var/www/resource-generator/code/wsgi.py
 echo "from runtime.config import app_config as config" >> /var/www/resource-generator/code/wsgi.py
+echo "os.environ['http_proxy'] = ''" >> /var/www/resource-generator/code/wsgi.py
+echo "os.environ['https_proxy'] = ''" >> /var/www/resource-generator/code/wsgi.py
 echo "application = create_app(config)" >> /var/www/resource-generator/code/wsgi.py
 echo "if __name__ == '__main__':" >> /var/www/resource-generator/code/wsgi.py
 echo "    application.run(host='0.0.0.0')" >> /var/www/resource-generator/code/wsgi.py
